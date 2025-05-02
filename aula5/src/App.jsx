b997a77f361fbdf137068eccb3aa981ac294057d
eukellyanne <kellyannekaren23@gmail.com>
2025-05-02 16:32:23 -0300

    aula5

---
+++ aula5/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListaDeAluno from './components/ListaDeAlunos'
+
+const App = () => {
+  return (
+    <div>
+      <ListaDeAluno />
+    </div>
+  )
+}
+
+export default App
---
+++ aula5/src/components/ListaDeAlunos.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+
+const ListaDeAluno = () => {
+
+  const [alunos, setAlunos] = useState([])
+
+  useEffect(() => {
+      async function fetchData() {
+        const response = await fetch("http://localhost:3000/alunos")
+        const data = await response.json()
+        setAlunos(data)
+      }
+
+      fetchData()
+
+  }, [])
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>Matricula:</th>
+          <th>Nome:</th>
+          <th>Email:</th>
+        </thead>
+        <tbody>
+          {
+            alunos.map((aluno) => (
+              <tr key={aluno.id}>
+                  <td>{aluno.id}</td>
+                  <td>{aluno.nome}</td>
+                  <td>{aluno.email}</td>
+              </tr>
+            ))
+          }
+
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default ListaDeAluno
